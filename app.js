2789257597db1a4e53554e09099245761e66d01d
aman <amanthapliyal14@gmail.com>
2021-04-08 10:56:30 +0530

    added routes controller related to aith

--- app.js
+++ app.js
@@ -3,9 +3,21 @@ const express = require("express")
 const homeroutes=require("./routes/homepage")
 const authRoutes=require("./routes/authentication")
 const err=require("./routes/error")
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const app = express()
 const port = process.env.PORT || 3000
+
+const User = require('./models/users');
+
+const MONGODB_URI =
+  '';
+
+  const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+  });
 
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -14,7 +26,27 @@ const viewsPath = path.join(__dirname, "views")
 app.set('view engine', 'ejs');
 app.set('views', viewsPath)
 
+app.use(
+    session({
+      secret: 'my secret',
+      resave: false,
+      saveUninitialized: false,
+      store: store
+    })
+  );
+  
 
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use(homeroutes);
 app.use(authRoutes);
--- controller/authentication.js
+++ controller/authentication.js
@@ -1,3 +1,4 @@
+const User=require('../models/users')
 
 exports.login=(req,res,next)=>{
     res.render('auth/login',{
@@ -8,5 +9,43 @@ exports.login=(req,res,next)=>{
 exports.signup=(req,res,next)=>{
     res.render('auth/signup',{
 
+    })
+}
+
+exports.postLogin=(req,res,next)=>{
+
+}
+
+exports.postSignup = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const confirmPassword = req.body.confirmPassword;
+    User.findOne({ email: email })
+      .then(userDoc => {
+        if (userDoc) {
+          return res.redirect('/');
+        }
+        const user = new User({
+          email: email,
+          password: password,
+          
+        });
+        return user.save();
+      })
+
+      .then(result => {
+        res.redirect('/');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+  
+
+
+exports.postLogout=(req,res,next)=>{
+    req.session.destroy(err=>{
+        console.log(err)
+        res.redirect('/')
     })
 }
