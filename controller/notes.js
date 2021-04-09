3808a4651c71c14af8e2839049f4d54a3d0b1c4d
Akash190301 <46964720+Akash190301@users.noreply.github.com>
2021-04-09 12:06:44 +0530

    Notes Section

--- controller/notes.js
+++ controller/notes.js
@@ -1,9 +1,13 @@
 
 exports.getnotes=(req,res,next)=>{
     let notes={
-        courseimage:["https://images.pexels.com/photos/3007370/pexels-photo-3007370.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500","https://images.pexels.com/photos/3861964/pexels-photo-3861964.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500","https://images.pexels.com/photos/2061168/pexels-photo-2061168.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500","https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500","https://images.pexels.com/photos/92904/pexels-photo-92904.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"],
-        coursename:["BCA","BSC IT","BTECH","BSC CS"],
-        coursedescription:["Bahelor Of Computer Appliations","Bachelor Of Information Security","Bachelor of Technology","Bachelor of Computer Science"],
+        courseimage:["https://images.pexels.com/photos/2061168/pexels-photo-2061168.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        "https://images.pexels.com/photos/2061168/pexels-photo-2061168.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        "https://images.pexels.com/photos/2061168/pexels-photo-2061168.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        "https://images.pexels.com/photos/2061168/pexels-photo-2061168.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        "https://images.pexels.com/photos/2061168/pexels-photo-2061168.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500","https://images.pexels.com/photos/2061168/pexels-photo-2061168.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"],
+        coursename:["BCA","BSC IT","BTECH","BSC CS", "MCA"],
+        coursedescription:["Bahelor Of Computer Appliations","Bachelor Of Information Security","Bachelor of Technology","Bachelor of Computer Science", "Master of Computer Application"],
         courselink:[""]
 
     }
